62d5b1723f35639f9672fed28d61501753a3eb64
obojoboi <boojelohne@hotmail.no>
2022-10-14 10:10:51 +0200

    player
    fiksa movement

---
+++ Assets/boje scripts/Player_script.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_script : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float RunSpeed = 10;
+    public float x;
+    public float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+        //rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       x = Input.GetAxis("Horizontal");
+       
+       y = Input.GetAxis("Vertical");
+       rb.velocity = new Vector2(x, y) * RunSpeed;
+      
+    }
+}
