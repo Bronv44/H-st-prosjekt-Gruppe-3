6d29042b3bb592a840508ad92c71c97281f692e7
obojoboi <boojelohne@hotmail.no>
2022-10-21 12:34:47 +0200

    update player
    ja

--- Assets/boje scripts/Player_script.cs
+++ Assets/boje scripts/Player_script.cs
@@ -5,13 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class Player_script : MonoBehaviour
 {
+    public enum MagicState { water, fix, shoot };
+    public MagicState magicState = MagicState.water;
+    private int magicState2;
+
+
     private Rigidbody2D rb;
+
+    public GameObject WaterSpell;
+    public Transform WaterSpellSpawn;
 
     public float RunSpeed = 10;
     public float x;
     public float y;
 
-    public Animator anim;
+    public Vector3 cursorPos;
+
+    //public Animator anim;
 
 
     // Start is called before the first frame update
@@ -25,14 +35,62 @@ public class Player_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
         x = Input.GetAxis("Horizontal");
-       
-       y = Input.GetAxis("Vertical");
-       rb.velocity = new Vector2(x, y) * RunSpeed;
 
-        anim.SetTrigger("Water");
-      
+        y = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(x, y) * RunSpeed;
+
+
+        //Debug.Log(anim.SetTrigger("Water"));
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //set spell 1 active
+            magicState = MagicState.water;
+            Debug.Log("spell");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            //set spell 2 active
+            magicState = MagicState.fix;
+            Debug.Log("Spell");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            //set spell 3 active
+            magicState = MagicState.shoot;
+            Debug.Log("Spell");
+        }
+
+
+        switch (magicState)
+        {
+            case MagicState.water:
+                //activate water code
+                Water();
+                break;
+            case MagicState.fix:
+                //activate fix code
+                break;
+            case MagicState.shoot:
+                //activate shoot code
+                break;
+        }
+
+    }
+
+
+    public void Water()
+    {
+        if (Input.GetMouseButtonDown(1))
+       
+        {
+            //Instantiate (WaterSpellPrefab, WaterSpawn.position, Quaternion.identity);
+        }
+
     }
 
 }
+
